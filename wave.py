f2b4d3f9a538c5b78585154e02947f867dad2ad5
noah-smith0921 <nhslogger@gmail.com>
2013-10-10 22:40:35 -0400

    deleted old useless code

--- wave.py
+++
@@ -1,9 +0,0 @@
-import sys
-import wave
-import math
-import struct
-import random
-import argparse
-from itertools import *
-def white_noise(amplitude=0.5):
-    return (float(amplitude) * random.uniform(-1, 1) for _ in count(0))
